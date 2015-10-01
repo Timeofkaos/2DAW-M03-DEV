335047c042f614b28293f99cc04342b4b6f917db
Timeofkaos <time.of.kaos@gmail.com>
2015-10-01 18:57:42 +0200

    Commit in Dev Environment

---
+++ HelloWorld.java
@@ -0,0 +1,8 @@
+//package cat.iesjoaquimmir.myapp.views;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+        System.out.println("Hello World! \n I'm Eric!! \n");		
+    }	
+}
